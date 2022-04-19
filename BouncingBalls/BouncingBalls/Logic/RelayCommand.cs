877b57b3c24427cc4f52d6787ec6b4bd54283147
Jakub Czyzewski <236513@edu.p.lodz.pl>
2022-04-19 11:34:59 +0200

    Presentation layer prototype

---
+++ BouncingBalls/BouncingBalls/Logic/RelayCommand.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace BouncingBalls.Logic
+{
+    public class RelayCommand : ICommand
+    {
+        #region private
+
+        private readonly Action _execute;
+        private readonly Func<bool> _canExecute;
+
+        #endregion private
+
+        #region constructors
+
+        public RelayCommand(Action execute, Func<bool> canExecute = null)
+        {
+            _execute = execute ?? throw new ArgumentNullException(nameof(execute));
+            _canExecute = canExecute;
+        }
+
+        #endregion constructors
+
+        #region ICommand
+
+        public bool CanExecute(object parameter)
+        {
+            if (_canExecute == null)
+                return true;
+            if (parameter == null)
+                return _canExecute();
+            return _canExecute();
+        }
+
+        public virtual void Execute(object parameter)
+        {
+            _execute();
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        #endregion ICommand
+
+        #region API
+
+        internal void RaiseCanExecuteChanged()
+        {
+            CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+        }
+
+        #endregion API
+    }
+}
---
+++ BouncingBalls/BouncingBalls/Presentation/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System.Windows;
+
+namespace BouncingBalls.Presentation
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ApplyButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            StartButton.IsEnabled = true;
+        }
+
+        private void StartButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            ApplyButton.IsEnabled = false;
+            StopButton.IsEnabled = true;
+            NumberInput.IsEnabled = false;
+        }
+
+        private void StopButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            StopButton.IsEnabled = false;
+            ApplyButton.IsEnabled = true;
+            StartButton.IsEnabled = false;
+            NumberInput.IsEnabled = true;
+        }
+    }
+}
